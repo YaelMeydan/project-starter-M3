622e2aa5d2f0ae69021376cd95ede5623f8d2619
YaelMeydan <yael.meydan@gmail.com>
2025-04-07 18:05:10 +0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import "dotenv/config";
+
+import { createServer } from "http";
+import mongoose from "mongoose";
+import { app } from "./app";
+
+const server = createServer(app);
+const port = process.env.PORT || 8090;
+
+async function init() {
+    await mongoose.connect(process.env.CONNECTION_STRING!, {
+        dbName: "intM3" ,
+    });
+
+    server.listen(port, () => console.log(`Server listening on port ${port}`));
+}
+
+init();
