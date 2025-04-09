d0133e24049039eee25ab307b264474f3626e32a
YaelMeydan <yael.meydan@gmail.com>
2025-04-09 18:19:09 +0300

    fix 1

--- public/index.ts
+++ public/index.ts
@@ -1,5 +1,5 @@
 export async function initApp(root: HTMLPreElement) {
-    const res = await fetch('/api');
+    const res = await fetch('/');
     const data = await res.json();
 
     root.textContent = JSON.stringify(data, null, 2);
